0ae30bc9cbead7ad83e17dad038566dd1efe43a0
Richard Russell <richardr39@gmail.com>
2018-11-11 18:28:35 +0000

    refactor: initial use of cmake

--- src/stdafx.cpp
+++ src/stdafx.cpp
@@ -2,7 +2,11 @@
 // satpi.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
+#ifdef USE_PRECOMPILED_HEADER
+
 #include "stdafx.h"
+
+#endif
 
 // TODO: reference any additional headers you need in STDAFX.H
 // and not in this file
